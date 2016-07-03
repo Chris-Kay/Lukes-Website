10447db623f2ae8f8694e48fe2c58222ce340148
Chris Kay <chris.kay01@bbc.co.uk>
2016-07-03 13:58:51 +0100

    Using require js

--- js/agency.js
+++ js/agency.js
@@ -1,10 +1,5 @@
-/*!
- * Agency v1.0.x (http://startbootstrap.com/template-overviews/agency)
- * Copyright 2013-2016 Start Bootstrap
- * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
- */
+var $ = require('jquery');
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -8,6 +8,9 @@
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
+
+var classie = require('./classie');
+
 var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
