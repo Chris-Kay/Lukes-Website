02bece377190db1650c64f6d76e354c603336f18
Chris Kay <chris.kay01@bbc.co.uk>
2016-07-17 13:52:47 +0100

    Updated contact me

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,7 +23,7 @@ define(['require', 'jqBootstrapValidation'], function (require) {
                   firstName = name.split(' ').slice(0, -1).join(' ');
               }
               $.ajax({
-                  url: "././mail/contact_me.php",
+                  url: "../mail/contact_me.php",
                   type: "POST",
                   data: {
                       name: name,
